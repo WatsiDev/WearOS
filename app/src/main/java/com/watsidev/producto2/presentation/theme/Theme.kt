8c5680ea24c840f26dcde7187d8f8ed25419d162
WatsiDev <jairvazquez604@gmail.com>
2025-07-11 18:16:02 -0600

    Initialize Android Wear project with basic structure and resources

---
+++ app/src/main/java/com/watsidev/producto2/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.watsidev.producto2.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun Producto2Theme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
